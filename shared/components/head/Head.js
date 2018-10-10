79681632bf32c547f0f4261a9e65c9a93f01532b
Mevan Abeydeera <mevanabeydeera@gmail.com>
2018-10-10 11:51:39 +0400

    CHORE: Add OpenSans Italic font for input label

--- shared/components/head/Head.js
+++ shared/components/head/Head.js
@@ -10,7 +10,7 @@ const Head = ({ description, title, ...props}) => (
         <meta name="description" content={ description } />
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
         <link rel="icon" href="/static/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,600i" rel="stylesheet" />
     </NextHead>
 );
 
