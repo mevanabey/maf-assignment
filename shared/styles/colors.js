8db0ae9f3eee14b44a6a2a2d9acb3b63b6185c60
Mevan Abeydeera <mevanabeydeera@gmail.com>
2018-10-10 11:47:10 +0400

    CHORE: Add red color

--- shared/styles/colors.js
+++ shared/styles/colors.js
@@ -4,11 +4,13 @@ export default {
     lightblue: '#f0f7fe',
     lightgrey: '#f5f5f5',
     white: '#fff',
+    lightred: '#fff0f1',
 
     font: {
         black: '#000',
         blue: '#05539f',
         darkgrey: '#414141',
         lightgrey: '#ddd',
+        red: '#f6303f',
     },
 };
